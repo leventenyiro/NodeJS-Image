56c7c102f3687604f74a57fc268497d9afc73f0f
leventenyiro <nyiro.levente@gmail.com>
2020-10-31 18:53:05 +0100

    add parameter

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const router = require("./router")
+const parameter = require("./parameter.json")
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use("/", router)
 
-app.listen(8080, () => {
+app.listen(parameter.app.port, () => {
     console.log("Running...")
 })
--- models/Database.js
+++ models/Database.js
@@ -1,17 +1,19 @@
+const parameter = require("../parameter.json")
+
 class Database {
     constructor() {
         var mysql = require("mysql")
         this.conn = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "",
-            database: "restapi"
+            host: parameter.db.host,
+            user: parameter.db.user,
+            password: parameter.db.password,
+            database: parameter.db.database
         })
         this.hashedId = ""
     }
 
     generateNewHashedId(table) {
-        this.hashedId = require("crypto").randomBytes(3).toString("hex")
+        this.hashedId = require("crypto").randomBytes(parameter.db.cryptSize).toString("hex")
         var sql = `SELECT COUNT(*) AS count FROM ${table} WHERE id = "${this.hashedId}"`
         this.conn.query(sql, (err, result) => {
             if (err) throw err
