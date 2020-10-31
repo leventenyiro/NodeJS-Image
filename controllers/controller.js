4b019bec13a58930a5fecb428612857fcde53291
leventenyiro <nyiro.levente@gmail.com>
2020-10-31 18:48:43 +0100

    add insert

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,19 @@
 const path = require("path")
+const Database = require("../models/Database")
 
 exports.getImage = (req, res) => {
     res.sendFile(path.resolve("./images/table.png"))
 }
 
 exports.addImage = (req, res) => {
-    console.log(req.file)
+    //console.log(req.file)
     if (req.file == undefined)
         res.send("Error")
-    else
-        res.send("Successful")
+    else {
+        const db = new Database()
+        db.addImage(req, (result) => {
+            res.send(result)
+            db.end()
+        })
+    }
 }
--- models/Database.js
+++ models/Database.js
@@ -10,6 +10,15 @@ class Database {
         this.hashedId = ""
     }
 
+    generateNewHashedId(table) {
+        this.hashedId = require("crypto").randomBytes(3).toString("hex")
+        var sql = `SELECT COUNT(*) AS count FROM ${table} WHERE id = "${this.hashedId}"`
+        this.conn.query(sql, (err, result) => {
+            if (err) throw err
+            else if (result[0].count == 1) this.generateNewHashedId(table);
+        })
+    }
+
     getImages(callback) {
         var sql = "SELECT * FROM image"
         this.conn.query(sql, (err, result) => {
@@ -21,6 +30,20 @@ class Database {
         var sql = `SELECT * FROM image WHERE id = ${req.params.id}`
         this.conn.query(sql, (err, result) => {
             return callback(result[0])
+        })
+    }
+
+    addImage(req, callback) {
+        this.generateNewHashedId("image")
+        var sql = `INSERT INTO image (id, file) VALUES (
+            "${this.hashedId}",
+            "${req.file.filename}"
+        )`
+        this.conn.query(sql, (err, result) => {
+            if (err)
+                callback("Error")
+            else
+                callback("Successful")
         })
     }
 
